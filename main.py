05f4d5fbe4b7a7e9974f4458a9dbac896ae6b4d8
Zach Schwartz <104332807+ZachatorCodes@users.noreply.github.com>
2024-03-21 22:43:18 +0000

    Allow looping of gameplay

--- main.py
+++ main.py
@@ -31,27 +31,37 @@ import random
 option_images = [rock, paper, scissors]
 option_text = ["rock", "paper", "scissors"]
 
+play_again = "y"
+
 print("Welcome to the Rock, Paper, Scissors game!")
 print(rock, paper, scissors)
-print("This game has been sponsored by Bunk, the Fattest Cat West of the Mississippi.\n")
+print("This game has been sponsored by Bunk, the Fattest Cat West of the Mississippi.")
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-opponent_choice = random.randint(0, 2)
+def play():
+  user_choice = int(input("\nWhat do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+  opponent_choice = random.randint(0, 2)
+  
+  print(f"\nYou chose: {option_text[user_choice]}")
+  print(option_images[user_choice])
+  print(f"Opponent chose: {option_text[opponent_choice]}")
+  print(option_images[opponent_choice])
+  
+  if user_choice < 0 or user_choice > 2:
+    print("Input not recognized. You lose!")
+  elif user_choice == opponent_choice:
+    print("Tie game!")
+  elif user_choice == 0 and opponent_choice == 2:
+    print("You win!")
+  elif user_choice == 1 and opponent_choice == 0:
+    print("You win!")
+  elif user_choice == 2 and opponent_choice == 1:
+    print("You win!")
+  else:
+    print("You lose!")
 
-print(f"\nYou chose: {option_text[user_choice]}")
-print(option_images[user_choice])
-print(f"Opponent chose: {option_text[opponent_choice]}")
-print(option_images[opponent_choice])
+while play_again.lower() == "y":
+  play()
+  play_again = input("\nDo you want to play again? Type 'Y' or 'N': ")
+  if play_again.lower() == "n":
+    print("\nGoodbye!")
 
-if user_choice < 0 or user_choice > 2:
-  print("Input not recognized. Goodbye!")
-elif user_choice == opponent_choice:
-  print("Tie game!")
-elif user_choice == 0 and opponent_choice == 2:
-  print("You win!")
-elif user_choice == 1 and opponent_choice == 0:
-  print("You win!")
-elif user_choice == 2 and opponent_choice == 1:
-  print("You win!")
-else:
-  print("You lose!")
